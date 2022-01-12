4cccf62dbebead3a999b8de406da7579edbb4c3f
LeeChiuLan <magicq2005@yaoo.com.tw>
2022-01-12 21:28:41 +0800

    darknet_deep_sort_main.py

---
+++ tracking/darknet_deep_sort_main.py
@@ -0,0 +1,212 @@
+
+from yolov3_deepsort.deep_sort import nn_matching
+from yolov3_deepsort.deep_sort.detection import Detection
+from yolov3_deepsort.deep_sort.tracker import Tracker
+from yolov3_deepsort.tools import generate_detections as gdet
+
+from ctypes import *
+import random
+import os
+import cv2
+import time
+import darknet
+import argparse
+from threading import Thread, enumerate
+from queue import Queue
+
+
+def parser():
+    parser = argparse.ArgumentParser(description="YOLO Object Detection")
+    parser.add_argument("--input", type=str, default=0,
+                        help="video source. If empty, uses webcam 0 stream")
+    parser.add_argument("--out_filename", type=str, default="",
+                        help="inference video name. Not saved if empty")
+    parser.add_argument("--weights", default="yolov4.weights",
+                        help="yolo weights path")
+    parser.add_argument("--dont_show", action='store_true',
+                        help="windown inference display. For headless systems")
+    parser.add_argument("--ext_output", action='store_true',
+                        help="display bbox coordinates of detected objects")
+    parser.add_argument("--config_file", default="./cfg/yolov4.cfg",
+                        help="path to config file")
+    parser.add_argument("--data_file", default="./cfg/coco.data",
+                        help="path to data file")
+    parser.add_argument("--thresh", type=float, default=.25,
+                        help="remove detections with confidence below this value")
+    return parser.parse_args()
+
+
+def str2int(video_path):
+    """
+    argparse returns and string althout webcam uses int (0, 1 ...)
+    Cast to int if needed
+    """
+    try:
+        return int(video_path)
+    except ValueError:
+        return video_path
+
+
+def check_arguments_errors(args):
+    assert 0 < args.thresh < 1, "Threshold should be a float between zero and one (non-inclusive)"
+    if not os.path.exists(args.config_file):
+        raise(ValueError("Invalid config path {}".format(os.path.abspath(args.config_file))))
+    if not os.path.exists(args.weights):
+        raise(ValueError("Invalid weight path {}".format(os.path.abspath(args.weights))))
+    if not os.path.exists(args.data_file):
+        raise(ValueError("Invalid data file path {}".format(os.path.abspath(args.data_file))))
+    if str2int(args.input) == str and not os.path.exists(args.input):
+        raise(ValueError("Invalid video path {}".format(os.path.abspath(args.input))))
+
+
+def set_saved_video(input_video, output_video, size):
+    fourcc = cv2.VideoWriter_fourcc(*"MJPG")
+    fps = int(input_video.get(cv2.CAP_PROP_FPS))
+    video = cv2.VideoWriter(output_video, fourcc, fps, size)
+    return video
+
+
+def video_capture(frame_queue, darknet_image_queue):
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frame_resized = cv2.resize(frame_rgb, (width, height),
+                                   interpolation=cv2.INTER_LINEAR)
+        frame_queue.put(frame_resized)
+        darknet.copy_image_from_bytes(darknet_image, frame_resized.tobytes())
+        darknet_image_queue.put(darknet_image)
+    cap.release()
+
+
+def inference(darknet_image_queue, detections_queue, fps_queue):
+    while cap.isOpened():
+        darknet_image = darknet_image_queue.get()
+        prev_time = time.time()
+        detections = darknet.detect_image(network, class_names, darknet_image, thresh=args.thresh)
+        detections_queue.put(detections)
+        fps = int(1/(time.time() - prev_time))
+        fps_queue.put(fps)
+        print("FPS: {}".format(fps))
+        darknet.print_detections(detections, args.ext_output)
+    cap.release()
+
+
+def drawing(frame_queue, detections_queue, fps_queue):
+    random.seed(3)  # deterministic bbox colors
+    video = set_saved_video(cap, args.out_filename, (width, height))
+    while cap.isOpened():
+        frame_resized = frame_queue.get()
+        detections = detections_queue.get()
+        fps = fps_queue.get()
+        if frame_resized is not None:
+            image = darknet.draw_boxes(detections, frame_resized, class_colors)
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            if args.out_filename is not None:
+                video.write(image)
+            if not args.dont_show:
+                cv2.imshow('Inference', image)
+            if cv2.waitKey(fps) == 27:
+                break
+    cap.release()
+    video.release()
+    cv2.destroyAllWindows()
+
+def split_darknet_detections(detections):
+    bboxs=[]
+    confidences=[]
+    labels=[]
+    for label, confidence, bbox in detections:
+        #bboxs.append(bbox)  # this bbox's (x,y) is the center point
+        _box=[]   # this _box's (x,y) is the top-left point
+        x = int(bbox[0]-bbox[2]/2)
+        _box.append(x)
+        y = int(bbox[1]-bbox[3]/2)
+        _box.append(y)
+        _box.append(bbox[2])
+        _box.append(bbox[3])
+        bboxs.append(_box)
+        confidences.append(confidence)
+        labels.append(label)
+    return labels,confidences,bboxs
+			
+def detect_tracking(frame_queue, detections_queue, fps_queue):
+    # generate different colors for different classes  
+    classesNum = len(class_names)
+    # ----------------------------------
+    # tracking with deep_sort
+    # ----------------------------------
+    # Definition of the parameters
+    tracking = True
+    max_cosine_distance = 0.5
+    nn_budget = None
+    nms_max_overlap = 1.0
+ 
+    # Deep SORT
+    model_filename = 'yolov3_deepsort/model_data/mars-small128.pb'
+    encoder = gdet.create_box_encoder(model_filename, batch_size=1)
+    metric = nn_matching.NearestNeighborDistanceMetric("cosine", max_cosine_distance, nn_budget)
+    tracker = Tracker(metric,max_age=100)
+
+    random.seed(3)  # deterministic bbox colors
+    video = set_saved_video(cap, args.out_filename, (width, height))
+    while cap.isOpened():
+        frame_resized = frame_queue.get()
+        detections = detections_queue.get()
+        fps = fps_queue.get()
+        if frame_resized is not None:
+            image = darknet.draw_boxes(detections, frame_resized, class_colors)
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            labels, confidences, boxes = split_darknet_detections(detections)
+            names = labels
+            if tracking:
+                features = encoder(image, boxes)
+                detections = [Detection(bbox, score, class_name, feature) for bbox, score, class_name, feature in zip(boxes, confidences, names, features)]
+            if tracking:
+                tracker.predict()
+                tracker.update(detections)
+                for track in tracker.tracks:
+                    if not track.is_confirmed() or track.time_since_update > 1:
+                        continue
+                    bbox = track.to_tlbr()
+                    class_name = track.get_class()
+                    color = class_colors[class_name]
+                    #cv2.rectangle(image, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), color, 2)
+                    cv2.rectangle(image, (int(bbox[0]), int(bbox[1]-30)), (int(bbox[0])+(len(class_name)+len(str(track.track_id)))*17, int(bbox[1])), color, -1)
+                    cv2.putText(image, class_name + "-" + str(track.track_id),(int(bbox[0]), int(bbox[1]-10)),0, 0.75, (255,255,255),2)
+            
+            if args.out_filename is not None:
+                video.write(image)
+            if not args.dont_show:
+                cv2.imshow('Tracking', image)
+            if cv2.waitKey(fps) == 27:
+                break
+    cap.release()
+    video.release()
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    frame_queue = Queue()
+    darknet_image_queue = Queue(maxsize=1)
+    detections_queue = Queue(maxsize=1)
+    fps_queue = Queue(maxsize=1)
+
+    args = parser()
+    check_arguments_errors(args)
+    network, class_names, class_colors = darknet.load_network(
+            args.config_file,
+            args.data_file,
+            args.weights,
+            batch_size=1
+        )
+    # Darknet doesn't accept numpy images.
+    # Create one with image we reuse for each detect
+    width = darknet.network_width(network)
+    height = darknet.network_height(network)
+    darknet_image = darknet.make_image(width, height, 3)
+    input_path = str2int(args.input)
+    cap = cv2.VideoCapture(input_path)
+    Thread(target=video_capture, args=(frame_queue, darknet_image_queue)).start()
+    Thread(target=inference, args=(darknet_image_queue, detections_queue, fps_queue)).start()
+    Thread(target=detect_tracking, args=(frame_queue, detections_queue, fps_queue)).start()
